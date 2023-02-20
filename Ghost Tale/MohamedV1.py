7e8a7be355b52b96e851817041930858455a8603
xeIam <114408204+xeIam@users.noreply.github.com>
2023-02-20 14:26:18 +0100

    Update MohamedV1.py
    j'ai rajouté ma touche à la ligne 149

--- Ghost Tale/MohamedV1.py
+++ Ghost Tale/MohamedV1.py
@@ -61,6 +61,12 @@ def getMagicList():
 #Initialise les premier phantomes de debut de partie et sauvegarde la ou les fantomes apparaissent
 #Pour chaque joueur dans une clé "ghostPopLocation"
 def setInitialGhostsAndPopSpot():
+    """
+    spawns the ghost of each player (free of charges :D) which has 100 health points (HP)
+    
+    returns:
+        Players: each player has now one ghost to their name in their own dictionary
+    """
   ghost1 = lines()[3].split()
   ghost2 = lines()[4].split()
   Players[1]["ghosts"][(int(ghost1[1]), (int(ghost1[2])))] =  100
@@ -73,6 +79,12 @@ def setInitialGhostsAndPopSpot():
 
 #create board list and print board with ressources bar
 def InitialBoardDisplay(MAP = getMap()):
+    """
+    displays the board of the game
+    
+    returns:
+        BOARD: board of the game
+    """
     BOARD = [[X+1,Y+1]for X in range(MAP[0]) for Y in range(MAP[1])]
     for cell in BOARD :
         if (cell[1]+cell[0])%((2+getMap()[1]+getMap()[0])/2) == 0:
@@ -90,8 +102,17 @@ def InitialBoardDisplay(MAP = getMap()):
     return BOARD
 #1 -ADD Ghost to players dict  and reduce magic points
 def Invokeghost (Players,PlayerID):
-    if Players[PlayerID]["magic"] >= 30 and not[Players[PlayerID]["ghostPopLocation"] in Players[PlayerID]["ghostsLocationList"]]:
-        Players[PlayerID]["magic"] -= 30
+    """
+    each player can choose whether or not they want to buy another ghost for 300 magic points
+    
+    parameters:
+        PlayerID: ID of the player who wants to buy a ghost (int)
+        
+    return:
+        Players: newly bought ghost(s) is added to the player's dictionary
+    """
+    if Players[PlayerID]["magic"] >= 300 and not[Players[PlayerID]["ghostPopLocation"] in Players[PlayerID]["ghostsLocationList"]]:
+        Players[PlayerID]["magic"] -= 300
         Players[PlayerID]["ghosts"][(Players[PlayerID]["ghostPopLocation"][0],Players[PlayerID]["ghostPopLocation"][1])] = 100
     return Players
 
@@ -102,9 +123,19 @@ def Healghost(Players,PlayerID,targetGhost,amount):
        Players[PlayerID]["magic"] -= (amount*2)
     return Players
 
-#3- Collecte the magic if ghost on spot and delete the magic cell from
+#3- Collect the magic if ghost on spot and delete the magic cell from
 #Magic Dictionnary and Magic list
 def collectMagic (PlayersID,targetGhost):
+    """
+    if the ghost of a player steps on a tile filled with magic, the magic is collected by the player which will therefore remove the magic from the tile
+    
+    parameters:
+        PlayersID: ID of the player (int)
+        targetGhost: ghost that stepped on the magic-filled tile (str)
+        
+    returns:
+        Players: the collected magic will be added to the player's dictionary
+    """
     if targetGhost in getMagicList() :
         Magicdict =  getMagicDict()
         Magiclist = getMagicList()
@@ -115,15 +146,35 @@ def collectMagic (PlayersID,targetGhost):
         return Players
 #4- MAKE A MOVE YOU STUPID GHOST <3, changes PLAYERS dict
 #Para: Takes in current and futur location
-# Changes the dict and moves the emo boiz
+# Changes the dict and moves the emo boiz before they start crying
 #requiers
 def moveGhost(ghostLocation,futureGhostLocation,PlayerID,Players):
+    """
+    move the ghost of a player according to the chosen direction
+    
+    parameters:
+        ghostLocation: location of the ghost (int)
+        futureGhostLocation: future location of the ghost after it got moved by the player (int)
+        PlayerID: ID of the player who wants to move its ghost (int)
+    
+    return:
+        Players: the player's dictionary is updated with its ghost new location
+    """
     curr = ghostLocation
     Players[PlayerID]["ghostsLocationList"]
 
 
 #5- Regen magic points to the player at the end of the round
 def regenPlayer(PlayerID,Players) :
+    """
+    automatically give 10HP to each player at the end of each turn, plus 1 additional HP per surviving ghosts
+    
+    parameters:
+        PlayersID: ID of the player which will receive the HP (int)
+    
+    returns:
+        Players: the player's dictionary is updated with the added HP
+    """
     Players[PlayerID]["magic"] += 10 
     return Players[PlayerID]["magic"]
 
